808a4dc7bd3b04e0c6c6d8193cc6fa73b55c688e
Moaz Malik <moaz007malik@gmail.com>
2023-02-27 15:36:09 +0500

    Panaverse Assignment

---
+++ Question 21/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let city = [
+    { name: "Lahore", population: "11.13 Million", country: "Pakistan" },
+    { name: "Karachi", population: "14.91 Million", country: "Pakistan" },
+    { name: "Islamabad", population: "9.21 Million", country: "Pakistan" },
+    { name: "Pishawar", population: "9.32 Million", country: "Pakistan" },
+    { name: "Kasur", population: "8.32 Million", country: "Pakistan" }
+];
+console.log(city);
