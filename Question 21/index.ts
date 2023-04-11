70a5362d7f5593743862b99f27bd709863350a86
Moaz Malik <69762233+Moaz007Malik@users.noreply.github.com>
2023-04-11 19:30:33 +0500

    Add files via upload

--- Question 21/index.ts
+++ Question 21/index.ts
@@ -1,14 +1,28 @@
-interface cities{
-    name: string;
-    population: string;
-    country: string;
-}
-
-let city:cities[] = [
-    { name: "Lahore", population: "11.13 Million", country:"Pakistan" },
-    { name: "Karachi", population: "14.91 Million", country:"Pakistan" },
-    { name: "Islamabad", population: "9.21 Million", country:"Pakistan" },
-    { name: "Pishawar", population: "9.32 Million", country: "Pakistan"},
-    { name: "Kasur", population: "8.32 Million", country: "Pakistan"}
-]
-console.log(city);
+let cities = [
+    {city1: {
+      name: "Lahore",
+      population: "19 Million",
+      country: "Pakistan"
+    }},
+    {city2: {
+        name: "Karachi", 
+        population: "14.91 Million", 
+        country:"Pakistan"
+    }},
+    {city3: {
+        name: "Islamabad", 
+        population: "9.21 Million", 
+        country:"Pakistan"
+    }},
+    {city4: {
+        name: "Pishawar", 
+        population: "9.32 Million", 
+        country: "Pakistan"
+    }},
+    {city5: { 
+        name: "Kasur", 
+        population: "8.32 Million", 
+        country: "Pakistan"
+    }}
+] 
+console.log(cities)
