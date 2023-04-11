b0b53517649815a589b0629236aa7ab7331a46ce
Moaz Malik <69762233+Moaz007Malik@users.noreply.github.com>
2023-04-11 19:44:34 +0500

    Add files via upload

---
+++ Question 22/index.js
@@ -0,0 +1,32 @@
+"use strict";
+let /*cities[]*/ cities = [
+    { city1: {
+            name: "Lahore",
+            population: "19 Million",
+            country: "Pakistan"
+        } },
+    { city2: {
+            name: "Karachi",
+            population: "14.91 Million",
+            country: "Pakistan"
+        } },
+    { city3: {
+            name: "Islamabad",
+            population: "9.21 Million",
+            country: "Pakistan"
+        } },
+    { city4: {
+            name: "Pishawar",
+            population: "9.32 Million",
+            country: "Pakistan"
+        } },
+    { city5: {
+            name: "Kasur",
+            population: "8.32 Million",
+            country: "Pakistan"
+        } }
+];
+console.log(cities);
+/*Conversion of type 'string' to type 'string[]' may be a mistake because neither type
+sufficiently overlaps with the other. If this was intentional, convert the expression
+to 'unknown' first. */ 
