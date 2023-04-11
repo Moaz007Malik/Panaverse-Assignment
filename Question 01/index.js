9924b9de98bdc0c92f4ec7f5c10165ad96bcc789
Moaz Malik <69762233+Moaz007Malik@users.noreply.github.com>
2023-04-11 21:11:45 +0500

    Add files via upload

---
+++ Question 01/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//First of all I have installed Node.js from https://nodejs.org/en/
+//After installing Node.js, I have installed TypeScript from https://www.typescriptlang.org/download
+//After installing TypeScript, I have installed VS Code from https://www.vscode.com/
+//These three programs are being used in our codes.
+console.log("Everything is installed.");
